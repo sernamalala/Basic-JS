81bcbf2ff3a94d2724d1377be54839d11b0cc812
sernamalala <sernaprograms@gmail.com>
2024-02-15 22:18:58 +0200

    number guesser game in js

---
+++ Backend JS/Number Guesser/script.js
@@ -0,0 +1,73 @@
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNumber = 1;
+let randomNum = Math.floor(Math.random() * (9 + 1));
+// Write your code below:
+
+const generateTarget = () => {
+
+    return randomNum;
+
+}
+
+const compareGuesses = (humanGuess, computerGuess, secretTarget) => {
+
+    let outcome;
+    if (humanGuess === computerGuess) {
+
+        outcome = true;
+    }
+
+    if (humanGuess > secretTarget && computerGuess > secretTarget) {
+        let humanValue = humanGuess - secretTarget;
+        let computerValue = computerGuess - secretTarget;
+
+        if (humanValue < computerValue) {
+            outcome = true;
+        }
+        else {
+            outcome = false;
+        }
+    }
+
+
+    else if (humanGuess > secretTarget && computerGuess < secretTarget) {
+        let humanValue = humanGuess - secretTarget;
+        let computerValue = secretTarget - computerGuess;
+
+        if (humanValue < computerValue) {
+            outcome = true;
+        }
+        else {
+            outcome = false;
+        }
+    }
+
+    else if (humanGuess < secretTarget && computerGuess < secretTarget) {
+        let humanValue = secretTarget - humanGuess;
+        let computerValue = secretTarget - computerGuess;
+
+        if (humanValue < computerValue) {
+            outcome = true;
+        }
+        else {
+            outcome = false;
+        }
+    }
+
+    else if (humanGuess < secretTarget && computerGuess > secretTarget) {
+        let humanValue = secretTarget - humanGuess;
+        let computerValue = computerGuess - secretTarget;
+
+        if (humanValue < computerValue) {
+            outcome = true;
+        }
+        else {
+            outcome = false;
+        }
+    }
+
+    return outcome;
+
+}
+
